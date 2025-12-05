7a51f6b137684d3e02e509463da7ca1e0e1b5c74
ddkiss <film@whu.edu.cn>
2025-12-06 01:25:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -16,10 +16,10 @@ class Config:
     LEVERAGE = float(os.getenv("LEVERAGE", "1.0"))
     BALANCE_PCT = float(os.getenv("BALANCE_PCT", "0.8"))
     STOP_LOSS_PCT = float(os.getenv("STOP_LOSS_PCT", "0.01"))
-    COOL_DOWN = int(os.getenv("COOL_DOWN", "30"))
+    COOL_DOWN = int(os.getenv("COOL_DOWN", "180"))
     # [新增] 超时止损时间 (秒)
     # 这里会优先读取 .env 中的 STOP_LOSS_TIMEOUT，如果没填则默认为 120
-    STOP_LOSS_TIMEOUT = int(os.getenv("STOP_LOSS_TIMEOUT", "120"))
+    STOP_LOSS_TIMEOUT = int(os.getenv("STOP_LOSS_TIMEOUT", "600"))
     # Taker 费率 (0.012% = 0.00012)
     TAKER_FEE_RATE = float(os.getenv("TAKER_FEE_RATE", "0.00018"))
 
